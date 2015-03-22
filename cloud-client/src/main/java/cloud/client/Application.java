839b0c4341f4489781cc324a21220ed75e314c8f
Josh Long <josh@joshlong.com>
2015-03-22 02:11:10 -0400

    moved to new release

--- cloud-client/src/main/java/cloud/client/Application.java
+++ cloud-client/src/main/java/cloud/client/Application.java
@@ -2,25 +2,21 @@ package cloud.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         Environment environment = SpringApplication.run(Application.class, args).getBean(Environment.class);
         String projectName = environment.getProperty("configuration.projectName");
-        System.out.println( projectName) ;
+        System.out.println(projectName);
     }
-/*
+
     @Value("${configuration.projectName}")
     void setProjectName(String projectName) {
         System.out.println("setProjectName('" + projectName + ");");
-    }*/
+    }
 
 }
